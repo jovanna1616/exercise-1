5235511182b089bf51f82742188862bcfc9a4c2b
jovanna1616 <jovana.r@vivifyideas.com>
2019-12-02 08:39:28 +0100

    1st task - Recursive components.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li>Root component - independed content</li>
+      <Folder :config="config"></Folder>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Folder from './components/Folder.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Folder
+  },
+  data () {
+    return {
+      config: {
+        title: 'It\'s shocking what you can find in stores now days.',
+        children: [{
+          title: 'Especially in outlet malls. They do have their positives and negatives, but overall they strike a bad cord with me.',
+          children: [{
+            title: 'I never found anything hot there, mostly just neutral staff.',
+            children: [{
+              title: 'Plus most stores just charge too much. I miss Circuit City.',
+              children: [{
+                title: 'I came here so I could feel grounded.',
+                children: [{
+                  title: 'Those are all painfull old. Don\'t you know any current pouns?',
+                  children: [{
+                    title: 'Ohm my God. Will you guys cut it out?',
+                    children: [{
+                      title: 'Ouch, that hz.',
+                      children: [{
+                        title: 'C-C-C-CITCUIT BREAKER',
+                        children: []
+                      }]
+                    }]
+                  }]
+                }]
+              }]
+            }]
+          }]
+        }]
+      }
+    }
   }
 }
 </script>
@@ -24,5 +58,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+ul {
+  list-style: none;
 }
 </style>
